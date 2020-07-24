26fa145308469f21e5213c43caffd3e5c3be369c
Luciano Ratamero <luciano@ratamero.com>
2020-07-24 18:07:25 -0300

    Add sample on how to use SCSS inside components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
-<style>
+<style lang="scss">
   h1,
+  h2,
   figure,
   p {
     text-align: center;
@@ -11,6 +12,23 @@
     text-transform: uppercase;
     font-weight: 700;
     margin: 0 0 0.5em 0;
+  }
+
+  h2 {
+    font-size: 2.4em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  $rainbow-colors: violet, indigo, blue, green, yellow, orange, red;
+
+  // thanks to https://codepen.io/cimmanon/pen/yoCDG for the inspiration!
+  .rainbow-text {
+    @for $i from 1 through length($rainbow-colors) {
+      span:nth-child(#{length($rainbow-colors)}n + #{$i}) {
+        color: nth($rainbow-colors, $i);
+      }
+    }
   }
 
   figure {
@@ -28,9 +46,8 @@
   }
 
   @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
+    h1 { font-size: 4em; }
+    h2 { font-size: 3.2em;}
   }
 </style>
 
@@ -40,9 +57,13 @@
 
 <h1>Great success!</h1>
 
+<h2 class="rainbow-text">
+  <span>W</span><span>i</span><span>t</span><span>h</span>&nbsp;<span>S</span><span>A</span><span>S</span><span>S</span><span>!</span>
+</h2>
+
 <figure>
   <img alt="Success Kid" src="successkid.jpg" />
-  <figcaption>Have fun with Sapper!</figcaption>
+  <figcaption>Have fun with Sapper and SASS!</figcaption>
 </figure>
 
 <p>
